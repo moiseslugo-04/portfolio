19e4762cc0e77210500e872b4860116b989634c9
Moises <moiseslugo9134@gmail.com>
2026-09-15 15:27:01 -0300

    refactor: refactor social links CRUD and add Axios to profile settings

--- src/features/auth/hooks/useLogin.ts
+++ src/features/auth/hooks/useLogin.ts
@@ -1,33 +1,41 @@
 'use client'
 
 import { useState } from 'react'
+
 import { useForm } from '@features/auth/hooks/useForm'
+
 import { loginSchema, LoginSchema } from '@features/auth/schema/auth'
+
 import { useRouter } from 'next/navigation'
+
 import { api } from '@/lib/api'
+
 import axios from 'axios'
+
 export function useLogin() {
   const [loading, setLoading] = useState(false)
+
   const [error, setError] = useState<string | null>(null)
+
   const router = useRouter()
 
   const form = useForm<LoginSchema>({
     schema: loginSchema,
+
     defaultValues: {
       identifier: '',
       password: '',
     },
   })
+
   const handleSubmit = form.handleSubmit(async (data) => {
     setLoading(true)
+
     setError(null)
 
     try {
       await api.post('/auth/login', data)
-<<<<<<< HEAD
 
-=======
->>>>>>> 28ad57f (refactor: refactor social links CRUD and add Axios to profile settings)
       router.push('/admin')
     } catch (error) {
       if (axios.isAxiosError(error) && error.response?.status === 401) {
